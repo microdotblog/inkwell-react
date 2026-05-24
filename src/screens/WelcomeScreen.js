279b4167cfb7e0353fbdf356b9f1e07aec1270aa
Manton Reece <manton@me.com>
2026-05-24 10:53:21 -0500

    Enabled highlighting for iOS.

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -28,7 +28,6 @@ import AuthCard from '../components/auth/AuthCard';
 import PrimaryButton from '../components/auth/PrimaryButton';
 import Auth, { MICRO_BLOG_SUBSCRIPTION_REQUIRED_MESSAGE } from '../stores/Auth';
 import AppStore from '../stores/App';
-import { APPLE_SECTION_313 } from '../config';
 import { getAuthTheme } from '../theme/authTheme';
 import { createScaledTextStyles } from '../theme/textScale';
 
@@ -357,12 +356,8 @@ function WelcomeScreen({ isDark = false }) {
               ]}
             >
               Inkwell is a feed reader that syncs with Micro.blog.
-              {!APPLE_SECTION_313 ? (
-                <>
-                  {'\n\n'}
-                  Make highlights to remember passages later or to blog quotes from them.
-                </>
-              ) : null}
+              {'\n\n'}
+              Make highlights to remember passages later.
             </Text>
           </Animated.View>
 
